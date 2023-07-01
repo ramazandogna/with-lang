63568a1c545c89d3444e79f12bad46c4e6abcd0f
Ramazan Doğan <doganrmzn40@gmail.com>
2023-07-01 14:03:41 +0300

    App has created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './Assets/Styles/global.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div>
+         <p>Hello world</p>
+         <h2>Hello World2</h2>
+         <h3>Hello World2</h3>
+         <div>div</div>
+      </div>
+   );
 }
 
 export default App;
